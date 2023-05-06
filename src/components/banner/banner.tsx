2583ad9c865c35c1c6b794db137ea0a6d0f77082
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-05-06 16:50:01 +0300

    add banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Container from "../container/Container";
 
 type BannerProps = {};
 
 const Banner: React.FC<BannerProps> = () => {
   return (
-    <main>
-      <div>
-        <div className="bg-green h-20 w-full shadow text-white p-8 jmb-8"></div>
-      </div>
-    </main>
+    <div className="bg-green  w-full shadow text-white p-8 jmb-8">
+      <Container>
+        <h1 className="drob-shadow text-6xl font-bold text-center">Conduit</h1>
+        <p className="text-2xl font-light text-center">
+          A place to share your knowledge.
+        </p>
+      </Container>
+    </div>
   );
 };
 
